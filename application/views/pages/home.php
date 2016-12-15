85c256a005772817d445dc7f2e966816bccb853b
nairda29 <adrian.guillermo16@gmail.com>
2016-12-15 10:19:01 +0800

    initial layout fix

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,87 +5,74 @@
     </div>
   </div>
 </div>
-<!-- <div class="jumbotron text-center">
-  <h1 id="part_name">Participant Name</h1>
-</div> -->
-
 <div class="container">
   <div class="row">
     <div class="col-sm-8">
       <div class="jumbotron text-center">
         <h1 id="part_name">Participant Name</h1>
-
       </div>      
-    </div>
-    <div class="col-sm-4">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>PRIZE</th>
-            <th>WINNER</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>John</td>
-            <td>Doe</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-          </tr>
-          <tr>
-            <td>John</td>
-            <td>Doe</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <div class="row">
-    <div class="col-sm-6">
-      <!-- <form role="form"> -->
-      <?php echo form_open('raffle/create') ?>
-      <?php echo validation_errors(); ?>
-      <div class="form-group">
-        <label for="name">Participant Name:</label>
-        <input type="name" class="form-control" id="name" name="name">
+      <div class="row">
+        <div class="col-sm-6">
+          <button type="button" class="btn btn-success btn-block" id="roll_button">Start Rolling!</button>
+        </div>
+        <div class="col-sm-6">
+          <button type="button" class="btn btn-danger btn-block" id="stop_button">STOP!</button>
+        </div>
       </div>
-      <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-user"></span>Add Participant</button>
-    </form>
-    <br>
- <!--    <table class="table table-hover">
-      <thead>Name of Participant</thead>
-      <tbody>
-        <?php 
-        // foreach ($participant_list as $row) {
-        //   echo "<tr><td>" . 
-        //   $row['name'].
-        //   "</td></tr>";
-        // }
-        ?>
-      </tbody>
-    </table> -->
+      <div class="row">
+        <div class="col-lg-12">
+          <?php 
+          $attributes = array('class' => 'form-inline', 'role'=>'form' );
+          echo form_open('raffle/create', $attributes);
+          echo validation_errors(); ?>
+          <div class="form-group">
+            <label for="name">Participant Name:</label>
+            <input type="name" class="form-control" id="name" name="name">
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-user"></span>Add Participant</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
-  <div class="col-sm-6">
-    <button type="button" class="btn btn-success btn-lg" id="roll_button">Start Rolling!</button>
-    <button type="button" class="btn btn-danger btn-lg" id="stop_button">STOP!</button>
+  <div class="col-sm-4">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>PRIZE</th>
+          <th>WINNER</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+        </tr>
+        <tr>
+          <td>Mary</td>
+          <td>Moe</td>
+        </tr>
+        <tr>
+          <td>July</td>
+          <td>Dooley</td>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+        </tr>
+        <tr>
+          <td>Mary</td>
+          <td>Moe</td>
+        </tr>
+        <tr>
+          <td>July</td>
+          <td>Dooley</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </div>
+</div>
+<div class="container" style="background-color: lavender">
 </div>
 <script type="text/javascript">
   participant_array = new Array();
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap-theme.min.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/mycss.css"); ?>" />
 	<title><?php echo $page_title ?></title>
 </head>
 <body>
